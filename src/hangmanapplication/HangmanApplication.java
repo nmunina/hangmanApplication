3513595dd2ffd251650ffd64dceb995b5045c3ec
Natalia Munina <nataly.munina@gmail.com>
2020-04-21 08:34:27 +0200

    Initial commit

---
+++ src/hangmanapplication/HangmanApplication.java
@@ -0,0 +1,10 @@
+package hangmanapplication;
+// blondiebytes 2015 (c)
+// author katrynhodge
+
+public class HangmanApplication {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
